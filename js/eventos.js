5664adb184be3e9d6a7bdc37c991265a8e5561f0
zoequiroz3 <126729283+zoequiroz3@users.noreply.github.com>
2023-03-08 15:07:57 -0300

    Add files via upload

---
+++ js/eventos.js
@@ -0,0 +1,4 @@
+
+document.getElementById('DatosC').onclick= function(){
+    document.getElementById("mensaje").innerHTML = "Muchas gracias por interesarte en mi. Mi telefono es +54 (717) 498-7232 y mi email es steven.taylor@example.com, espero tu propuesta."
+}
